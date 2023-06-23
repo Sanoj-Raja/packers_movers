5f3da60d747f46d4afa5cd2e99a872a976a0a09a
Sanoj Raja <pimen07860@gmail.com>
2023-06-23 18:39:57 +0530

    Added quotation screen.

---
+++ lib/app/modules/add_quotation_screen/add_quotation_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddQuotationScreen extends StatelessWidget {
+  const AddQuotationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Add Quote');
+  }
+}
